1c4b668099c2302a7b32cb05c437cba889ff53b6
Effi Ovadia <Effi.Ovadia@GMail.com>
2024-12-23 01:24:54 +0200

    modified:   PowerPlans/PowerPlans/ImportPowerPlan.ps1 	modified:   PowerPlans/RestorePowerPlan.ps1

--- PowerPlans/PowerPlans/ImportPowerPlan.ps1
+++ PowerPlans/PowerPlans/ImportPowerPlan.ps1
@@ -1,13 +1,11 @@
-# Power Plans Restore for Windows 11 
-
-#@ Balanced
+## Balanced
 powercfg -import .\Balanced.pow 381b4222-f694-41f0-9685-ff5bb260df2e
 
-#@ Power saver
+## Power saver
 powercfg -import .\Power_Saver.pow a1841308-3541-4fab-bc81-f71556f20b4a
 
-#@ High performance
+## High performance
 powercfg -import .\High_Performance.pow 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
-#@ Ultimate Performance
+## Ultimate Performance
 powercfg -import .\Ultimate_Performance.pow e9a42b02-d5df-448d-aa00-03f14749eb61
--- PowerPlans/RestorePowerPlan.ps1
+++ PowerPlans/RestorePowerPlan.ps1
@@ -1,16 +1,16 @@
 # Power Plans Restore for Windows 11 
 
-#@ Rstore all defaults
+## Rstore all defaults
 powercfg -restoredefaultschemes
 
-#@ Balanced
+## Balanced
 powercfg -duplicatescheme 381b4222-f694-41f0-9685-ff5bb260df2e
 
-#@ Power saver
+## Power saver
 powercfg -duplicatescheme a1841308-3541-4fab-bc81-f71556f20b4a
 
-#@ High performance
+## High performance
 powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
-#@ Ultimate Performance
+## Ultimate Performance
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
