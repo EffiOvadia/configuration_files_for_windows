d659134cb91117b7b819a9331f68da11f788c0d3
Effi Ovadia <effi.ovadia@gmail.com>
2025-02-23 10:23:19 +0200

    deleted:    windows/PowerPlans/PowerPlans/ImportPowerPlan.ps1 	new file:   windows/PowerPlans/PowerPlans/import_power_plan.ps1 	renamed:    windows/PowerPlans/RestorePowerPlan.ps1 -> windows/PowerPlans/restore_power_plan.ps1

---
+++ windows/PowerPlans/PowerPlans/import_power_plan.ps1
@@ -0,0 +1,8 @@
+# Balanced
+powercfg -import .\blanced.pow 381b4222-f694-41f0-9685-ff5bb260df2e
+# Power saver
+powercfg -import .\power_saver.pow a1841308-3541-4fab-bc81-f71556f20b4a
+# High performance
+powercfg -import .\high_performance.pow 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+# Ultimate Performance
+powercfg -import .\ultimate_performance.pow e9a42b02-d5df-448d-aa00-03f14749eb61
